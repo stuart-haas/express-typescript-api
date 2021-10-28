c3b8affdc3c41e3bf2f3836b2acd2b056d7100d0
Stuart <stuart.haas2@gmail.com>
2021-10-28 00:42:46 -0400

    Add server interface

---
+++ src/app/interfaces/ServerInterface.ts
@@ -0,0 +1,6 @@
+import { Application } from 'express';
+
+export interface ServerInterface {
+  app: Application;
+  start(): void;
+}
--- src/boot/server.ts
+++ src/boot/server.ts
@@ -1,16 +1,17 @@
+import { ServerInterface } from 'app/interfaces/ServerInterface';
 import express from 'express';
 import { singleton } from 'tsyringe';
 
 @singleton()
-export class Server {
+export class Server implements ServerInterface {
   
-  public app: express.Application;
+  app: express.Application;
 
   constructor() {
     this.app = express();
   }
 
-  public start() {
+  public start(): void {
     this.app.listen(process.env.PORT, () => {
       console.log(
         `Application is up and running on port ${process.env.PORT}`
